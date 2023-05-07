22c9477ecf6abd7dee849d2099ed4918dc04e5bc
bunnyBites <s.mukeshkr481@gmail.com>
2023-05-07 20:14:48 +0530

    add question and state update functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const Quizzler());
@@ -29,21 +31,30 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> iconList = [];
+  List<String> quitionare = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    "A slug's blood is green.",
+  ];
+
+  int currentStep = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        const Expanded(
+        Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quitionare[currentStep],
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
@@ -53,7 +64,9 @@ class _QuizPageState extends State<QuizPage> {
         ),
         choiceButton("True", Colors.green),
         choiceButton("False", Colors.red),
+
         //TODO: Add a Row here as your score keeper
+        Row(children: iconList)
       ],
     );
   }
@@ -73,6 +86,27 @@ class _QuizPageState extends State<QuizPage> {
           ),
           onPressed: () {
             //The user picked true.
+            if (currentStep > 2) return;
+
+            setState(() {
+              currentStep += 1;
+            });
+
+            if (buttonLabel == "True") {
+              setState(() {
+                iconList.add(const Icon(
+                  Icons.check,
+                  color: Colors.green,
+                ));
+              });
+            } else {
+              setState(() {
+                iconList.add(const Icon(
+                  Icons.close,
+                  color: Colors.red,
+                ));
+              });
+            }
           },
         ),
       ),
@@ -81,7 +115,7 @@ class _QuizPageState extends State<QuizPage> {
 }
 
 /*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
+question1: , false,
 question2: 'Approximately one quarter of human bones are in the feet.', true,
 question3: 'A slug\'s blood is green.', true,
 */
